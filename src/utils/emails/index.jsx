e1cb6433486216befe63065c09e27f569f7effd3
francisguitt <francisguitt22209@gmail.com>
2024-03-21 18:24:52 -0300

    .

--- src/utils/emails/index.jsx
+++ src/utils/emails/index.jsx
@@ -1,6 +1,5 @@
 export const Email = {
     email: "contato@ebikeglobal.com.br",
-    // personal_email: "ericosantiago@uol.com.br"
-   personal_email: "francisguitt22209@gmail.com"
-//    personal_email: "guittdev22209@gmail.com"
+    personal_email: "ericosantiago@uol.com.br"
+
 }
