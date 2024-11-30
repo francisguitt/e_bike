306a7f83109ca6b3e534e625eb5407ba14e00922
Guitt Web Code <77627405+francisguitt@users.noreply.github.com>
2024-11-30 08:59:58 -0300

    Update index.jsx

--- src/utils/smtp/index.jsx
+++ src/utils/smtp/index.jsx
@@ -1,6 +1,6 @@
 export const smtpProvider = {
-    services_id: "service_19bmzpn",
-    template_id: "template_exatidd",
-    public_key: "Ouqxl9p-i4HConS3b"
+    services_id: "",
+    template_id: "",
+    public_key: ""
 }
 
